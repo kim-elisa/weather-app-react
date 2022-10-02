746397ade65dbebdc245ba38fa57221e06e19845
Kim Elisa Munnery <kimelisamunnery@gmail.com>
2022-10-02 16:50:10 +0100

    Added npm bootstrap and sorted HTML

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather />
+
+        <footer>
+          <a
+            href="https://github.com/kim-elisa/weather-app-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open source code
+          </a>{" "}
+          {""}
+          by {""}
+          <a
+            href="https://github.com/kim-elisa"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Kim E Munnery
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
