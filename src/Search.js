be776a172ac9d6840136f321bfd75bb5c13fe92d
Kim Elisa Munnery <kimelisamunnery@gmail.com>
2022-10-12 18:58:16 +0100

    added axios, fixed css and added state to Weather.js file

--- src/Search.js
+++ src/Search.js
@@ -5,32 +5,29 @@ import "./App.css";
 export default function Search() {
   return (
     <div className="container Search">
-      <form id="city-search-form">
+      <form className="city-search-form">
         <div className="row">
           <div className="col-2">
             <input
               type="button"
               value="📍"
-              id="current-location-button"
-              className="btn btn-warning"
+              className="btn btn-warning current-location-button"
             />
           </div>
           <div className="col-8">
             <input
               type="search"
-              className="form-control w-100"
+              className="form-control w-100 search-text-input"
               placeholder="Enter a city"
               autocomplete="off"
               autofocus="on"
-              id="search-text-input"
               aria-label="search"
             />
           </div>
           <div className="col-2">
             <button
               type="submit"
-              id="search-city-button"
-              className="btn btn-primary"
+              className="btn btn-primary search-city-button"
             >
               Search
             </button>
--- src/Weather.js
+++ src/Weather.js
@@ -1,66 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import Search from "./Search";
 
 import "./App.css";
 import "./Weather.css";
 
 export default function Weather() {
-  let weatherData = {
-    city: "Tokyo",
-    temperature: 22,
-    date: "Monday 19:00",
-    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
-    description: "Sunny",
-    wind: 2,
-  };
+  const [weatherData, setWeatherData] = useState({ ready: false });
 
-  let url = "#";
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: `Monday 19:00`,
+      description: response.data.weather[0].description,
+      imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
+  }
 
-  return (
-    <div className="container Weather">
-      <div className="card">
-        <div className="card-body full-app">
-          <div className="row">
-            <div className="col-7">
-              <div className="overview">
-                <h1 id="city">{weatherData.city}</h1>
-                <div className="sub-overview">
-                  <h2 id="current-date">{weatherData.date}</h2>
-                  <h3 id="weather-description">{weatherData.description}</h3>
-                  <p class="wind-speed">
-                    Wind: <span id="wind">{weatherData.wind}</span> m/s
-                  </p>
+  function search() {
+    const apiKey = "d1be4136ed4955ecd4ad578e1cdcae10";
+    let city = "Tokyo";
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="container Weather">
+        <div className="card">
+          <div className="card-body full-app">
+            <div className="row">
+              <div className="col-7">
+                <div className="overview">
+                  <h1 className="city">{weatherData.city}</h1>
+                  <div className="sub-overview">
+                    <h2 className="current-date">{weatherData.date}</h2>
+                    <h3 className="weather-description">
+                      {weatherData.description}
+                    </h3>
+                    <p className="wind-speed">
+                      Wind: <span>{Math.round(weatherData.wind)}</span> m/s
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-5">
+                <div className="overview-weather">
+                  <span className="temperature todays-temp">
+                    <strong>{Math.round(weatherData.temperature)}</strong>
+                  </span>
+                  <span className="units">
+                    <a href="#" className="celsius">
+                      °C
+                    </a>{" "}
+                    |{" "}
+                    <a href="#" className="fahrenheit">
+                      °F
+                    </a>
+                  </span>
+                  <div>
+                    <img
+                      src={weatherData.imgUrl}
+                      alt={weatherData.description}
+                      className="float-right today-weather-icon"
+                    ></img>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="col-5">
-              <div className="overview-weather">
-                <span className="temperature" id="todays-temp">
-                  <strong>{weatherData.temperature}</strong>
-                </span>
-                <span class="units">
-                  <a href={url} id="celsius">
-                    °C
-                  </a>{" "}
-                  |{" "}
-                  <a href={url} id="fahrenheit">
-                    °F
-                  </a>
-                </span>
-                <div>
-                  <img
-                    src={weatherData.imgUrl}
-                    alt="Sunny"
-                    id="today-weather-icon"
-                    className="float-right"
-                  ></img>
-                </div>
-              </div>
-            </div>
+            <Search />
           </div>
-          <Search />
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
 }
